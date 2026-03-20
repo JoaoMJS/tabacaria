895479afd1b08e7ffb569c5b0f86804737615539
Joao <jmsouza4002@gmail.com>
2026-03-20 11:36:14 -0300

    adição de conteudo, marca e categoria para edição

--- Produtos/editar_produto.php
+++ Produtos/editar_produto.php
@@ -13,26 +13,58 @@ $produto = $sql->fetch(PDO::FETCH_ASSOC);
 
 <form method="POST" action="editar_produto_action.php">
 
-<input type="hidden" name="id" value="<?php echo $produto['id']; ?>"/>
+  <input type="hidden" name="id" value="<?= $produto['id']; ?>">
 
-<label>
-Nome:<br>
-<input type="text" name="nome" value="<?php echo $produto['nome']; ?>"/>
-</label>
+  <label>
+    Nome:<br>
+    <input type="text" name="nome" value="<?= $produto['nome']; ?>">
+  </label>
+  <br><br>
 
-<br><br>
+  <label>
+    Preço:<br>
+    <input type="text" name="preco" value="<?= $produto['preco']; ?>">
+  </label>
+  <br><br>
 
-<label>
-Preço:<br>
-<input type="text" name="preco" value="<?php echo $produto['preco']; ?>"/>
-</label>
+  <label>
+    Imagem:<br>
+    <input type="text" name="imagem" value="<?= $produto['imagem']; ?>">
+  </label>
+  <br><br>
 
-<br><br>
+  <!-- campos de edição - descrição, conteudo, marca, categoria e imagem -->
 
-<input type="submit" value="Salvar alterações"/>
+  <label>
+    Descrição:<br>
+    <textarea name="descricao"><?= $produto['descricao'] ?? ''; ?></textarea>
+  </label>
+  <br><br>
+
+  <label>
+    Conteúdo:<br>
+    <input type="text" name="conteudo" value="<?= $produto['conteudo'] ?? ''; ?>">
+  </label>
+  <br><br>
+
+  <label>
+    Marca:<br>
+    <input type="text" name="marca" value="<?= $produto['marca'] ?? ''; ?>">
+  </label>
+  <br><br>
+
+  <label>
+    Categoria:<br>
+    <input type="text" name="categoria" value="<?= $produto['categoria'] ?? ''; ?>">
+  </label>
+  <br><br>
+
+  <label>
+    Imagem:<br>
+    <input type="text" name="imagem" value="<?= $produto['imagem'] ?? ''; ?>">
+  </label>
+  <br><br>
+
+  <input type="submit" value="Salvar">
 
 </form>
-
-<br>
-
-<a href="listar_produtos.php">Voltar</a>
--- Produtos/editar_produto_action.php
+++ Produtos/editar_produto_action.php
@@ -4,9 +4,36 @@ require '../config.php';
 $id = $_POST['id'];
 $nome = $_POST['nome'];
 $preco = $_POST['preco'];
+$imagem = $_POST['imagem'];
 
-$sql = $pdo->prepare("UPDATE produtos SET nome = ?, preco = ? WHERE id = ?");
-$sql->execute([$nome, $preco, $id]);
+$descricao = $_POST['descricao'];
+$conteudo = $_POST['conteudo'];
+$marca = $_POST['marca'];
+$categoria = $_POST['categoria'];
+
+$sql = $pdo->prepare("
+  UPDATE produtos 
+  SET 
+    nome = ?, 
+    preco = ?, 
+    imagem = ?, 
+    descricao = ?, 
+    conteudo = ?, 
+    marca = ?, 
+    categoria = ?
+  WHERE id = ?
+");
+
+$sql->execute([
+  $nome, 
+  $preco, 
+  $imagem,
+  $descricao,
+  $conteudo,
+  $marca,
+  $categoria,
+  $id
+]);
 
 header("Location: listar_produtos.php");
 exit;
