0d900ea2669685122fdf73ba5c4b34079d161c60
Joao <jmsouza4002@gmail.com>
2026-03-25 11:10:44 -0300

    listagem dos usuários

--- Usuarios/listar_usuarios.php
+++ Usuarios/listar_usuarios.php
@@ -1,44 +1,70 @@
 <?php
+session_start();
 require '../config.php';
 
-$lista = [];
+$usuarios = [];
 
-$sql = $pdo->query("SELECT * FROM usuario");
+$sql = $pdo->query("SELECT id, nome, email, criado_em FROM usuarios");
 
 if($sql->rowCount() > 0){
-    $lista = $sql->fetchAll(PDO::FETCH_ASSOC);
+  $usuarios = $sql->fetchAll(PDO::FETCH_ASSOC);
 }
 ?>
 
-<h2>Lista de Usuários</h2>
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+  <meta charset="UTF-8">
+  <title>Usuários</title>
+  <link rel="stylesheet" href="../Site/estilo.css">
+</head>
 
-<a href="cadastrar.php">Cadastrar novo usuário</a>
+<body>
 
-<br><br>
+<header class="topo">
+  <div class="container topo-interno">
+    <h1 class="logo">Usuários do Site</h1>
 
-<table border="1">
+    <nav class="menu">
+      <a href="../Site/index.php">Início</a>
+      <a href="../Site/produtos.php">Produtos</a>
 
-<tr>
-<th>ID</th>
-<th>Nome</th>
-<th>Email</th>
-<th>Ações</th>
-</tr>
+      <?php if(isset($_SESSION['usuario'])): ?>
+        <a href="logout.php">Sair</a>
+      <?php else: ?>
+        <a href="login.php">Login</a>
+      <?php endif; ?>
+    </nav>
+  </div>
+</header>
 
-<?php foreach($lista as $usuario): ?>
+<main class="container">
 
-<tr>
-<td><?php echo $usuario['id']; ?></td>
-<td><?php echo $usuario['nome']; ?></td>
-<td><?php echo $usuario['email']; ?></td>
+  <h2>Usuários cadastrados</h2>
 
-<td>
-<a href="editar_usuario.php?id=<?php echo $usuario['id']; ?>">Editar</a> |
-<a href="excluir_usuario.php?id=<?php echo $usuario['id']; ?>">Excluir</a>
-</td>
+  <table style="width:100%; margin-top:20px; border-collapse: collapse;">
 
-</tr>
+    <tr style="background:#222;">
+      <th style="padding:10px;">ID</th>
+      <th>Nome</th>
+      <th>Email</th>
+      <th>Data de criação</th>
+    </tr>
 
-<?php endforeach; ?>
+    <?php foreach($usuarios as $user): ?>
 
-</table>
+    <tr style="border-bottom:1px solid #333; text-align:center;">
+      <td style="padding:10px;"><?= $user['id']; ?></td>
+      <td><?= $user['nome']; ?></td>
+      <td><?= $user['email']; ?></td>
+      <td><?= $user['criado_em']; ?></td>
+    </tr>
+
+    <?php endforeach; ?>
+
+  </table>
+
+</main>
+
+</body>
+</html>
